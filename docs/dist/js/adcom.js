24b078a32c863ea1386ae42fa148db5cd97a8e55
Michael Strickland <michael.c.strickland@gmail.com>
2014-09-11 14:04:47 -0700

    updated docs and dist

--- docs/dist/js/adcom.js
+++ docs/dist/js/adcom.js
@@ -274,8 +274,12 @@
 
   Index.prototype.updateAtIndex = function (idx, item) {
     this.$items[idx] = item
-
-    if (this.rendered[idx]) this.rendered[idx].replaceWith(this.renderItem(item))
+    if (this.rendered[idx]) {
+      var original    = this.rendered[idx]
+      var replacement = this.renderItem(item)
+      original.replaceWith(replacement)
+      this.rendered[idx] = replacement
+    }
   }
 
   // Trigger definitions
@@ -414,7 +418,6 @@
   var Form = function (element, options) {
     this.options  = options
     this.$element = $(element)
-    this.source   = null
 
     this.show(typeof this.options.serialized === 'string' ? JSON.parse(this.options.serialized) : this.options.serialized)
   }
@@ -426,29 +429,25 @@
   }
 
   Form.prototype.show = function (data) {
-    this.source = data
-
     this.$element.trigger($.Event('show.adcom.form', { serialized: data }))
 
+    this.data = data
     this.$element[0].reset()
     this.$element.deserialize(data)
 
     this.$element.trigger($.Event('shown.adcom.form', { serialized: data }))
   }
   // alias
-  Form.prototype.deserialize = Form.prototype.show
 
   Form.prototype.submit = function () {
-    var serialized = this.serialize()
+    var attributes = this.serialize()
+    $.extend(attributes, {
+      relatedTarget: this.$element,
+      sourceElement: this.$element.data('adcom.form.sourceElement'),
+      sourceData:    this.$element.data('adcom.form.sourceData')
+    })
 
-    // 'submit' event already exists for actually submitting the form
-    // maybe we shouldn't use this name then?
-    // this.$element.trigger($.Event('submit.adcom.form', { serialized: data }))
-
-    // this.$element.trigger($.Event('update.adcom.index', { serialized: data }))
-    // this.$element.trigger($.Event('updated.adcom.index', { serialized: data }))
-
-    this.$element.trigger($.Event('submitted.adcom.form', serialized))
+    this.$element.trigger($.Event('submitted.adcom.form', attributes))
   }
 
   Form.prototype.serialize = function () {
@@ -519,13 +518,13 @@
     var $target    = $($this.data('target'))
     var $sourceKey = $this.data('source') || 'serialized'
 
-    var source = closestWithData($this, $sourceKey)
+    var source     = closestWithData($this, $sourceKey)
+    var serialized = source.data($sourceKey)
 
-    $target.data('adcom.form.trigger', $this)
-    $target.data('adcom.form.source', source)
-    $target.data('adcom.form.sourceKey', $sourceKey)
+    $target.data('adcom.form.sourceElement', source.clone(true, false))
+    $target.data('adcom.form.sourceData', serialized)
 
-    Plugin.call($target, 'show', source.data($sourceKey))
+    Plugin.call($target, 'show', serialized)
   })
 
   $(document).on('submit', 'form[data-control="form"]', function (e) {
@@ -533,16 +532,18 @@
     $(e.target).form('submit')
   })
 
-}(jQuery);
+  /**
+   * @author Kyle Florence <kyle[dot]florence[at]gmail[dot]com>
+   * @website https://github.com/kflorence/jquery-deserialize/
+   * @version 1.2.1
+   *
+   * Dual licensed under the MIT and GPLv2 licenses.
+   */
+  +function(i,b){var f=Array.prototype.push,a=/^(?:radio|checkbox)$/i,e=/\+/g,d=/^(?:option|select-one|select-multiple)$/i,g=/^(?:button|color|date|datetime|datetime-local|email|hidden|month|number|password|range|reset|search|submit|tel|text|textarea|time|url|week)$/i;function c(j){return j.map(function(){return this.elements?i.makeArray(this.elements):this}).filter(":input").get()}function h(j){var k,l={};i.each(j,function(n,m){k=l[m.name];l[m.name]=k===b?m:(i.isArray(k)?k.concat(m):[k,m])});return l}i.fn.deserialize=function(A,l){var y,n,q=c(this),t=[];if(!A||!q.length){return this}if(i.isArray(A)){t=A}else{if(i.isPlainObject(A)){var B,w;for(B in A){i.isArray(w=A[B])?f.apply(t,i.map(w,function(j){return{name:B,value:j}})):f.call(t,{name:B,value:w})}}else{if(typeof A==="string"){var v;A=A.split("&");for(y=0,n=A.length;y<n;y++){v=A[y].split("=");f.call(t,{name:decodeURIComponent(v[0]),value:decodeURIComponent(v[1].replace(e,"%20"))})}}}}if(!(n=t.length)){return this}var u,k,x,z,C,o,m,w,p=i.noop,s=i.noop,r={};l=l||{};q=h(q);if(i.isFunction(l)){s=l}else{p=i.isFunction(l.change)?l.change:p;s=i.isFunction(l.complete)?l.complete:s}for(y=0;y<n;y++){u=t[y];C=u.name;w=u.value;if(!(k=q[C])){continue}m=(z=k.length)?k[0]:k;m=(m.type||m.nodeName).toLowerCase();o=null;if(g.test(m)){if(z){x=r[C];k=k[r[C]=(x==b)?0:++x]}p.call(k,(k.value=w))}else{if(a.test(m)){o="checked"}else{if(d.test(m)){o="selected"}}}if(o){if(!z){k=[k];z=1}for(x=0;x<z;x++){u=k[x];if(u.value==w){p.call(u,(u[o]=true)&&w)}}}}s.call(this);return this}}(jQuery)
 
-/**
- * @author Kyle Florence <kyle[dot]florence[at]gmail[dot]com>
- * @website https://github.com/kflorence/jquery-deserialize/
- * @version 1.2.1
- *
- * Dual licensed under the MIT and GPLv2 licenses.
- */
-(function(i,b){var f=Array.prototype.push,a=/^(?:radio|checkbox)$/i,e=/\+/g,d=/^(?:option|select-one|select-multiple)$/i,g=/^(?:button|color|date|datetime|datetime-local|email|hidden|month|number|password|range|reset|search|submit|tel|text|textarea|time|url|week)$/i;function c(j){return j.map(function(){return this.elements?i.makeArray(this.elements):this}).filter(":input").get()}function h(j){var k,l={};i.each(j,function(n,m){k=l[m.name];l[m.name]=k===b?m:(i.isArray(k)?k.concat(m):[k,m])});return l}i.fn.deserialize=function(A,l){var y,n,q=c(this),t=[];if(!A||!q.length){return this}if(i.isArray(A)){t=A}else{if(i.isPlainObject(A)){var B,w;for(B in A){i.isArray(w=A[B])?f.apply(t,i.map(w,function(j){return{name:B,value:j}})):f.call(t,{name:B,value:w})}}else{if(typeof A==="string"){var v;A=A.split("&");for(y=0,n=A.length;y<n;y++){v=A[y].split("=");f.call(t,{name:decodeURIComponent(v[0]),value:decodeURIComponent(v[1].replace(e,"%20"))})}}}}if(!(n=t.length)){return this}var u,k,x,z,C,o,m,w,p=i.noop,s=i.noop,r={};l=l||{};q=h(q);if(i.isFunction(l)){s=l}else{p=i.isFunction(l.change)?l.change:p;s=i.isFunction(l.complete)?l.complete:s}for(y=0;y<n;y++){u=t[y];C=u.name;w=u.value;if(!(k=q[C])){continue}m=(z=k.length)?k[0]:k;m=(m.type||m.nodeName).toLowerCase();o=null;if(g.test(m)){if(z){x=r[C];k=k[r[C]=(x==b)?0:++x]}p.call(k,(k.value=w))}else{if(a.test(m)){o="checked"}else{if(d.test(m)){o="selected"}}}if(o){if(!z){k=[k];z=1}for(x=0;x<z;x++){u=k[x];if(u.value==w){p.call(u,(u[o]=true)&&w)}}}}s.call(this);return this}})(jQuery);
+  $('[data-control="form"]').form()
+
+}(jQuery);
 
 +function ($) {
   'use strict';
