50e09129004693556cc1ef473287287577524970
Michael Strickland <michael.c.strickland@gmail.com>
2014-09-17 13:12:05 -0700

    prep for rc1

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,6 +48,54 @@ module.exports = function (grunt) {
         dest: 'dist/css/<%= pkg.name %>.css'
       }
     },
+    csslint: {
+      options: {
+        csslintrc: 'less/.csslintrc'
+      },
+      dist: [
+        'dist/css/adcom.css'
+      ],
+      docs: {
+        options: {
+          ids: false,
+          'overqualified-elements': false
+        },
+        src: 'docs/assets/css/src/docs.css'
+      }
+    },
+    csscomb: {
+      options: {
+        config: 'less/.csscomb.json'
+      },
+      dist: {
+        expand: true,
+        cwd: 'dist/css/',
+        src: ['*.css', '!*.min.css'],
+        dest: 'dist/css/'
+      },
+      docs: {
+        src: 'docs/assets/css/src/docs.css',
+        dest: 'docs/assets/css/src/docs.css'
+      }
+    },
+    cssmin: {
+      options: {
+        compatibility: 'ie8',
+        keepSpecialComments: '*',
+        noAdvanced: true
+      },
+      minifyCore: {
+        src: 'dist/css/<%= pkg.name %>.css',
+        dest: 'dist/css/<%= pkg.name %>.min.css'
+      },
+      docs: {
+        src: [
+          'docs/assets/css/src/docs.css',
+          'docs/assets/css/src/pygments-manni.css'
+        ],
+        dest: 'docs/assets/css/docs.min.css'
+      }
+    },
     copy: {
       docs: {
         src: 'dist/*/*',
@@ -70,14 +118,16 @@ module.exports = function (grunt) {
 
   // CSS distribution task.
   grunt.registerTask('less-compile', ['less:compileCore']);
-  grunt.registerTask('dist-css', ['less-compile']);
+  grunt.registerTask('dist-css', ['less-compile', 'csscomb:dist', 'cssmin:minifyCore']);
 
   // Full distribution task.
-  grunt.registerTask('dist', ['clean:dist', 'dist-js', 'dist-css']);
+  grunt.registerTask('dist', ['clean:dist', 'dist-css', 'dist-js', 'dist-css']);
 
   // Docs task.
+  grunt.registerTask('docs-css', ['csscomb:docs', 'cssmin:docs']);
+  grunt.registerTask('lint-docs-css', ['csslint:docs']);
   grunt.registerTask('docs-js', ['uglify:docsJs']);
-  grunt.registerTask('docs', ['docs-js', 'clean:docs', 'copy:docs', 'copy:docs_license']);
+  grunt.registerTask('docs', ['docs-css', 'lint-docs-css', 'docs-js', 'clean:docs', 'copy:docs', 'copy:docs_license']);
 
   // Default task.
   grunt.registerTask('default', ['dist', 'docs']);
