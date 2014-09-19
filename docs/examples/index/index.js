1e50445311c4c48ab37188fe56cd65543a830640
Michael Strickland <michael.c.strickland@gmail.com>
2014-09-19 14:01:00 -0700

    fixed links to github assets in docs

--- docs/examples/index/index.js
+++ docs/examples/index/index.js
@@ -1,28 +1,4 @@
 $(document).ready(function() {
-  // (function index_pull_quote() {
-  //   $.getJSON('http://int-shared-data.nytimes.com/adcom/items.json', function(items) {
-  //     $('#index_pull_quote .index').index({
-  //       items: items,
-  //       pageSize: 3,
-  //       pagination: 'on',
-  //       template: $('#index_pull_quote .index').html()
-  //     });
-
-  //     $('[title]').tooltip();
-  //   });
-  // })();
-
-  // (function index_search() {
-  //   $.getJSON('http://int-shared-data.nytimes.com/adcom/items.json', function(items) {
-  //     $('#index_search .index').index({
-  //       items: items,
-  //       pageSize: 3,
-  //       pagination: 'on',
-  //       template: $('#index_search .index').html()
-  //     });
-  //   });
-  // })();
-
   $('body').scrollspy({
     target: '.bs-docs-sidebar',
     offset: 40
