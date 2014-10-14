79bf35877afc1b4cd244e9e223db2d2bc6917f86
Michael Strickland <michael.c.strickland@gmail.com>
2014-10-14 15:16:37 -0700

    docs dist

--- docs/dist/js/adcom.js
+++ docs/dist/js/adcom.js
@@ -12,10 +12,10 @@
     this.$items   = typeof this.options.items === 'string' ? JSON.parse(this.options.items) : this.options.items
     this.template = this.parseTemplate(this.options.template)
 
-    this.states   = this.options.states === 'string' ? this.options.states.split(/,\s*/) : this.options.states || []
+    this.states   = typeof this.options.states === 'string' ? this.options.states.split(/,\s*/) : this.options.states || []
     this.rendered = []
 
-    this.sort        = null
+    this.sort        = this.options.sort ? this.getSort.apply(this, $.map([this.options.sort], function (n) { return n })) : null
     this.filters     = {}
     this.currentPage = parseInt(this.options.currentPage || 1)
     this.pageSize    = parseInt(this.options.pageSize || 1)
